bc709b7210939ba73cda66e17b1bfc68e12e7ab5
TanyaRan <tatyanaran@gmail.com>
2021-04-21 23:55:06 +0300

    add Cart component, get images from robohash.com

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './card.css'
 
-const Card = () => {
+const Card = props => {
   return (
-    <div>
-      <h1>Card</h1>
+    <div className='card-container'>
+      <img
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+        alt='monster'
+      />
+      <h2>{props.monster.name}</h2>
+      <p>{props.monster.email}</p>
     </div>
   )
 }
